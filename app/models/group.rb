b747e7392eea86ec290ad1bf4a9315e967abe191
gitsuguru <sugusugu.ryouri@gmail.com>
2020-08-12 00:16:02 +0900

    GroupView

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,16 @@ class Group < ApplicationRecord
   has_many :memos
   
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
